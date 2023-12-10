0e6e2819bb720d427f605ece8681939e9c253447
gottagoa <136464755+gottagoa@users.noreply.github.com>
2023-12-10 19:29:26 +0600

    first_commit

---
+++ list_channelid.py
@@ -0,0 +1,28 @@
+'''
+List (by Channel ID):
+
+Описание: Этот запрос используется для получения информации о канале на YouTube, идентифицируемом уникальным 
+идентификатором канала, известным как Channel ID.
+Пример использования: Вы можете передать Channel ID в запросе и получить данные, такие как заголовок канала, описание, 
+дата создания и другие сведения о канале.
+'''
+
+from googleapiclient.discovery import build
+
+def get_channel_info_by_id(api_key, channel_id):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    
+    request = youtube.channels().list(
+        part='snippet,contentDetails,statistics',
+        id=channel_id
+    )
+
+    response = request.execute()
+    return response
+
+# Укажите свой API-ключ и Channel ID
+api_key = 'YOUR_API_KEY'
+channel_id = 'CHANNEL_ID'
+
+channel_info = get_channel_info_by_id(api_key, channel_id)
+print(channel_info)
---
+++ youtube1.py
@@ -0,0 +1,34 @@
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from googleapiclient.discovery import build
+import json
+
+# необходимо сначала установить библиотеку google-api-python-client: pip install google-api-python-client
+
+API_KEY=''
+# здесь представле ключ пользователя, зарегистрировавшийся в google cloude platform console
+
+
+def get_service():
+    service=build('youtube', 'v3', developerKey=API_KEY)
+    return service
+
+# Get channel info (title, desc, stats)
+def get_channel_info(channel_id=''):
+    r=get_service().channels().list(id=channel_id, part='snippet, statistics').execute()
+    print(r['items'][0]['snippet']['title'])
+    print(r['items'][0]['statistics']['viewcount'])
+
+
+# Get video info(title, stats, desc)
+def get_video_info(video_id=''):
+    r=get_service().videos().list(id=video_id, part='snippet,statistics').execute()
+    print(r['items'][0]['snippet']['title'])
+    print(r['items'][0]['statistics']['viewcount'])
+
+
+if __name__=='__main__' :
+    # get_channel_info('')
+    get_video_info('')
