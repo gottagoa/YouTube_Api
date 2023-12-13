7381a391e3e7336e4ff06e11065c064cce4858e2
gottagoa <136464755+gottagoa@users.noreply.github.com>
2023-12-13 17:01:12 +0600

    uploading video

--- api/duration_video.py
+++ api/duration_video.py
@@ -1,20 +1,20 @@
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-import os
-# находим продолжительность видео и всего плейлиста
-api_key='AIzaSyCbXuG-eNeZRxtv8Zv9lulG37HpL03Evyk'
+# from googleapiclient.discovery import build
+# from google.oauth2.credentials import Credentials
+# import os
+# # находим продолжительность видео и всего плейлиста
+# api_key='AIzaSyCbXuG-eNeZRxtv8Zv9lulG37HpL03Evyk'
 
-youtube=build('youtube', 'v3', developerKey=api_key)
-pl_request=youtube.playlistItems().list(
-    part='contentDetails',
-    playlistId='Pl-osiE80TeTsWmV9i9c58mdDCSskIFdD'
-)
+# youtube=build('youtube', 'v3', developerKey=api_key)
+# pl_request=youtube.playlistItems().list(
+#     part='contentDetails',
+#     playlistId='Pl-osiE80TeTsWmV9i9c58mdDCSskIFdD'
+# )
 
-pl_response=pl_request.execute()
+# pl_response=pl_request.execute()
 
-vid_ids=[]
-for item in pl_response['items']:
-    vid_ids.append(item['contentDetails']['videoId'])
+# vid_ids=[]
+# for item in pl_response['items']:
+#     vid_ids.append(item['contentDetails']['videoId'])
 
-print(','.join(vid_ids))
+# print(','.join(vid_ids))
 
