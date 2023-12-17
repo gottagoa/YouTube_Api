22d70fa8a38fdee6ff594c256d192c117b7c01e3
gottagoa <136464755+gottagoa@users.noreply.github.com>
2023-12-17 21:20:15 +0600

    updating

--- own_channel/oauth2_upload_video.py
+++ own_channel/oauth2_upload_video.py
@@ -1,17 +1,19 @@
 import os
+import socket
 import google.oauth2.credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 
+
 SCOPES=['https://www.googleapis.com/auth/youtube.upload']
 
-CLIENT_SECRET_FILE='client_secret.json'
+CLIENT_SECRET_FILE='/Users/ajzanylsabdanbekova/Desktop/python/youtubeapi/own_channel/client_secret.json'
 CHANNEL_ID='UCrOYjS2VKmDAy39AI2BCfKw'
 
 flow=InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-credentials=flow.run_console()
+credentials=flow.run_local_server()
 
 token_file='token.json'
 with open(token_file, 'w') as token:
@@ -45,7 +47,7 @@ def upload_video(youtube, file_path, title, description):
     )
     print(f"Video id {response['id']} was succesfully uploaded.")
 
-file_path='Users/ajzanylsabdanbekova/Desktop/lesson4-списки.mov'
+file_path='/Users/ajzanylsabdanbekova/Desktop/lesson4-списки.mov'
 title="Онлайн урок 4-Списки. Методы списков. Индексы."
 description='В онлайн уроке раскрыта тема работы и конструкции списков в Python, а также решены задачи'
 
