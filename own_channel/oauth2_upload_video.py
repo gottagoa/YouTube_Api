91ba1f99b689d385f07c4b8eb18ec4079e72ac5c
gottagoa <136464755+gottagoa@users.noreply.github.com>
2024-01-12 17:19:51 +0600

    changing uri

--- own_channel/oauth2_upload_video.py
+++ own_channel/oauth2_upload_video.py
@@ -6,14 +6,19 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 
-SCOPES=['https://www.googleapis.com/auth/youtube.upload']
+SCOPES=[
+    'https://www.googleapis.com/auth/youtube.force-ssl',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/youtube.upload',
+    'https://www.googleapis.com/auth/userinfo.email'
+]
 
 CLIENT_SECRET_FILE='/Users/ajzanylsabdanbekova/Desktop/python/youtubeapi/own_channel/client_secret.json'
 CHANNEL_ID='UCrOYjS2VKmDAy39AI2BCfKw'
 
 
 flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
-flow.redirect_uri = "http://localhost:9000"
+flow.redirect_uri = "https://oauth.pstmn.io/v1/callback"
 credentials = flow.run_local_server()
 
 
@@ -23,67 +28,35 @@ with open(token_file, 'w') as token:
 
 youtube=build('youtube', 'v3', credentials=credentials)
 
-# def upload_video(youtube, file_path, title, description):
-#     request_body={
-#         'snippet':{
-#             'title': title,
-#             'description':description,
-#             'tags': ['python', 'youtube', 'api'],
-#             'categoryId': '27'
-#         },
-#         'status': {
-#             'privacyStatus': 'private',
-#         }
-#     }
+def upload_video(youtube, file_path, title, description):
+    request_body={
+        'snippet':{
+            'title': title,
+            'description':description,
+            'tags': ['python', 'youtube', 'api'],
+            'categoryId': '27'
+        },
+        'status': {
+            'privacyStatus': 'private',
+        }
+    }
 
-#     media_file=MediaFileUpload(file_path)
+    media_file=MediaFileUpload(file_path)
 
-#     response=(
-#         youtube.videos()
-#         .insert(
-#             part='snippet, status',
-#             body=request_body,
-#             media_body=media_file,
-#         )
-#         .execute()
-#     )
-#     print(f"Video id {response['id']} was succesfully uploaded.")
+    response=(
+        youtube.videos()
+        .insert(
+            part='snippet, status',
+            body=request_body,
+            media_body=media_file,
+        )
+        .execute()
+    )
+    print(f"Video id {response['id']} was succesfully uploaded.")
 
-# file_path='/Users/ajzanylsabdanbekova/Desktop/lesson4-списки.mov'
-# title="Онлайн урок 4-Списки. Методы списков. Индексы."
-# description='В онлайн уроке раскрыта тема работы и конструкции списков в Python, а также решены задачи'
+file_path='/Users/ajzanylsabdanbekova/Desktop/lesson4-списки.mov'
+title="Онлайн урок 4-Списки. Методы списков. Индексы."
+description='В онлайн уроке раскрыта тема работы и конструкции списков в Python, а также решены задачи'
 
 # upload_video(youtube, file_path, title, description)
 
-def upload_thumbnail(self, video_id, file_path):
-        print('Uploading thumbnail...')
-        request = self.youtube.thumbnails().set(
-            videoId=video_id,
-            media_body=file_path
-        )
-        response = request.execute()
-        print(response)
-
-def upload_video(self, file_path, video_content):
-    body = dict(
-        snippet=dict(
-            title=video_content.title,
-            description=video_content.description,
-            tags=video_content.tags,
-            categoryId=video_content.category_id
-        ),
-
-        status=dict(
-            privacyStatus=video_content.privacy_status
-        )
-    )
-
-    insert_request = self.youtube.videos().insert(
-        part=",".join(body.keys()),
-        body=body,
-        media_body= MediaFileUpload(
-            file_path, chunksize=-1, resumable=True)
-    )
-
-    video_id = self.resumable_upload(insert_request)
-    self.upload_thumbnail(video_id, 'files/youtube/thumbnail.png')
